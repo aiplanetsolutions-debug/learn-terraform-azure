0d3c34dbd037410208243919ec75677a23989bfd
niyi <elemzy@yahoo.com>
2026-08-07 23:28:59 -0600

    update vm model and repush v3

--- variable.tf
+++ variable.tf
@@ -18,7 +18,7 @@ variable "eastus2_location" {
 
 variable "vm_size" {
   type    = string
-  default = "Standard_D2s_v4" # Originally from your parameter file
+  default = "Standard_D2s_v6" # Originally from your parameter file
 }
 
 variable "vm_name_1" {
